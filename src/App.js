ac904a2342f7dc7dda2cc1476ca999cf996cb428
krates98 <67364288+krates98@users.noreply.github.com>
2024-01-30 02:51:06 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [mexcData, setMexcData] = useState([]);
+  const [kucoinData, setKucoinData] = useState([]);
+  const [bitmartData, setBitmartData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://139.59.32.99:3000/mexc")
+      .then((response) => response.json())
+      .then((data) => setMexcData(data));
+
+    fetch("http://139.59.32.99:3000/kucoin")
+      .then((response) => response.json())
+      .then((data) => setKucoinData(data));
+
+    fetch("http://139.59.32.99:3000/bitmart")
+      .then((response) => response.json())
+      .then((data) => setBitmartData(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Mexc Data</h2>
+      {mexcData.map((client) => (
+        <div key={client.clientName}>
+          <p>Client Name: {client.clientName}</p>
+          {client.balances.map((balance) => (
+            <p key={balance.asset}>
+              Asset: {balance.asset}, Balance:{" "}
+              {parseFloat(balance.free) + parseFloat(balance.locked)}
+            </p>
+          ))}
+        </div>
+      ))}
+
+      <h2>Kucoin Data</h2>
+      {kucoinData.map((account) => (
+        <div key={account.accountLabel}>
+          <p>Client Name: {account.accountLabel}</p>
+          {Object.entries(account.balances).map(([asset, balance]) => (
+            <p key={asset}>
+              Asset: {asset}, Balance: {balance}
+            </p>
+          ))}
+        </div>
+      ))}
+
+      <h2>Bitmart Data</h2>
+      {bitmartData.map((asset) => (
+        <div key={asset.name}>
+          <p>Client Name: Bobc Bitmart</p>
+          <p>
+            Asset: {asset.name}, Balance: {asset.balance}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
